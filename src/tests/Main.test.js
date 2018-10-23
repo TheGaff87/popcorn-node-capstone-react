d7d28f343fa616ae90dfd76ada97b7f700e64555
Azureowl <maiaroche16@gmail.com>
2018-10-23 16:41:09 -0700

    Removed test item no longer needed

--- src/tests/Main.test.js
+++ src/tests/Main.test.js
@@ -5,8 +5,6 @@ import chai from 'chai';
 import chaiEnzyme from 'chai-enzyme';
 
 import Main from '../components/Main';
-import Header from '../components/Header';
-import Welcome from "../components/Welcome";
 import SearchForm from "../components/SearchForm";
 import Chat from "../components/Chat";
 import Player from "../components/Player";
@@ -14,8 +12,6 @@ import Player from "../components/Player";
 describe('<Main />', () => {
     it('renders <Main /> components', () => {
         const wrapper = shallow(<Main />);
-        expect(wrapper.find(Header)).to.have.lengthOf(1);
-        expect(wrapper.find(Welcome)).to.have.lengthOf(1);
         expect(wrapper.find(SearchForm)).to.have.lengthOf(1);
         expect(wrapper.find(Chat)).to.have.lengthOf(1);
         expect(wrapper.find(Player)).to.have.lengthOf(1);
