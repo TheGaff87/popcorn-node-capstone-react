8ef2384c6f1a1a854fe228862a349a1a4c08f86f
Azureowl <maiaroche16@gmail.com>
2018-10-27 14:23:33 -0700

    Added https to api_origin

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-exports.API_ORIGIN = process.env.REACT_APP_API_ORIGIN || 'http://popcorn-capstone-node.herokuapp.com';
+exports.API_ORIGIN = process.env.REACT_APP_API_ORIGIN || 'https://popcorn-capstone-node.herokuapp.com';
